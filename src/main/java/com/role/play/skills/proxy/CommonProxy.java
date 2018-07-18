672551d749110621dd8403d6e6d4a73b3a68e666
Garthi <garth@new-crusader.de>
2018-07-18 15:25:37 +0200

    small changes on CommonProxy and removed all unused code

--- src/main/java/com/role/play/skills/proxy/CommonProxy.java
+++ src/main/java/com/role/play/skills/proxy/CommonProxy.java
@@ -3,21 +3,13 @@ package com.role.play.skills.proxy;
 import com.role.play.skills.common.RecipeBookRemover;
 import com.role.play.skills.utilities.ConfigHelper;
 import com.role.play.skills.utilities.RecipeBookRemoverDatabase;
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.potion.Potion;
-import net.minecraft.stats.StatBase;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
-import net.minecraft.world.storage.WorldInfo;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.FMLLog;
@@ -30,7 +22,6 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import org.apache.logging.log4j.Level;
 
 import java.io.File;
-import java.util.List;
 
 /**
  * @author Martin "Garth" Zander <garth@new-crusader.de>
@@ -45,7 +36,6 @@ public class CommonProxy
         RecipeBookRemoverDatabase.init(new File(event.getModConfigurationDirectory(), "recipeBookRemoverDatabase.cfg"));
         
         MinecraftForge.EVENT_BUS.register(this);
-        FMLLog.log.log(Level.INFO, "<RRS> preInit:" + event.toString());
     }
     
     public void init(FMLInitializationEvent event)
@@ -88,70 +78,10 @@ public class CommonProxy
         recipeBookRemover.forPlayer((EntityPlayerMP)event.player);
     }
 
-
-    @SubscribeEvent
-    public static void registerPotions(RegistryEvent.Register<Potion> event)
-    {
-        FMLLog.log.log(Level.ERROR, "<RRS> registerPotions:" + event.getName().toString());
-    }
-
-    @SubscribeEvent
-    public static void registerItems(RegistryEvent.Register<Item> event)
-    {
-        FMLLog.log.log(Level.ERROR, "<RRS> registerItems:" + event.getName().toString());
-    }
-
-    @SubscribeEvent
-    public static void registerEnchantments(RegistryEvent.Register<Enchantment> event)
-    {
-        FMLLog.log.log(Level.ERROR, "<RRS> registerEnchantments:" + event.getName().toString());
-    }
-
-    @SubscribeEvent
-    public static void registerRecipes(RegistryEvent.Register<IRecipe> event)
-    {
-        FMLLog.log.log(Level.INFO, "<RRS> registerRecipes:" + event.toString());
-    }
-    
-    @SubscribeEvent
-    public void harvest(BlockEvent.HarvestDropsEvent event)
-    {
-        String playerName;
-        try {
-            playerName = event.getHarvester().getDisplayName().getFormattedText();
-        } catch (NullPointerException e) {
-            playerName = "World";
-        }
-        List<ItemStack> drops = event.getDrops();
-
-        for (ItemStack drop: drops) {
-            FMLLog.log.log(Level.INFO, String.format(
-                    "<RRS> harvest: %1$s have dropped %2$dx %3$s",
-                    playerName,
-                    drop.getCount(),
-                    drop.getUnlocalizedName()
-            ));
-        }
-        
-        // set drop chance to 0 :-)
-        event.setDropChance(0);
-    }
-
-    @SubscribeEvent
-    public void itemCrafted(net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemCraftedEvent event)
-    {
-        FMLLog.log.log(Level.INFO, "itemCrafted: " + event.crafting.getUnlocalizedName());
-    }
-
-    @SubscribeEvent
-    public void onItemPickupEvent(PlayerEvent.ItemPickupEvent event)
-    {
-        FMLLog.log.log(Level.INFO, "onItemPickupEvent: " + event.getOriginalEntity().getItem().getUnlocalizedName());
-    }
-    
     @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
-    public void itemSmelted(net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemSmeltedEvent event)
+    public void itemSmelted(PlayerEvent.ItemSmeltedEvent event)
     {
+        // TODO
         FMLLog.log.log(Level.INFO, "itemSmelted: " + event.smelting.getUnlocalizedName());
         //event.setCanceled(true);
         //event.player.addStat(new StatBase(), 1);
@@ -160,32 +90,15 @@ public class CommonProxy
     @SubscribeEvent
     public void blockBreak(BlockEvent.BreakEvent event)
     {
+        // TODO
         if (event.getPlayer() != null && !event.getPlayer().isCreative()
                 && event.getState().getBlock() == Blocks.WEB)
         {
-            event.setCanceled(true);
+            //event.setCanceled(true);
 
             // send message to player
             EntityPlayer p = event.getPlayer();
             p.sendMessage(new TextComponentString("That is not allowed"));
         }
     }
-    
-    /*@SubscribeEvent
-    public void registerRecipes(RegistryEvent.Register<IRecipe> event) {
-        IForgeRegistryModifiable modRegistry = (IForgeRegistryModifiable) event.getRegistry();
-        boolean hardOption = OresBase.config.getBoolean("RequireMillingFlour", "MILLING", false, "");
-        if(hardOption) {
-            RecipesUtils.RemoveRecipe(modRegistry, new ResourceLocation("minecraft:bread"), "Hard Ores");
-            RecipesUtils.RemoveRecipe(modRegistry, new ResourceLocation("minecraft:cookie"), "Hard Ores");
-            RecipesUtils.RemoveRecipe(modRegistry, new ResourceLocation("minecraft:cake"), "Hard Ores");
-        }
-        boolean stoneTools = OresBase.config.getBoolean("useDioriteStoneTools", "GENERAL", true, "If true, cobblestone cannot be used to create stone tools,\ninstead diorite is used. This prolongs the life of wood tools so it isn't \"make a wood pickaxe to\nmine 3 stone and upgrade.\"");
-        if(stoneTools) {
-            RecipesUtils.RemoveRecipe(modRegistry, new ResourceLocation("minecraft:stone_axe"), "Harder Ores");
-            RecipesUtils.RemoveRecipe(modRegistry, new ResourceLocation("minecraft:stone_pickaxe"), "Harder Ores");
-            RecipesUtils.RemoveRecipe(modRegistry, new ResourceLocation("minecraft:stone_shovel"), "Harder Ores");
-            RecipesUtils.RemoveRecipe(modRegistry, new ResourceLocation("minecraft:stone_hoe"), "Harder Ores");
-        }
-    }*/
 }
